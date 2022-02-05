ce8b185ce4be3ee0dcdf77b890b60d9f56093194
trojnartom <trojnar.t@hotmail.com>
2022-02-05 14:26:47 +0100

    Project Library. Book constructor and printInfo method.

--- src/main/java/trojnartom/javastart/library/Book.java
+++ src/main/java/trojnartom/javastart/library/Book.java
@@ -9,4 +9,17 @@ public class Book {
     String publisher;
     String isbn;
 
+    public Book(String title, String author, int releaseDate, int pages, String publisher, String isbn) {
+        this.title = title;
+        this.author = author;
+        this.releaseDate = releaseDate;
+        this.pages = pages;
+        this.publisher = publisher;
+        this.isbn = isbn;
+    }
+
+    public void printInfo() {
+        String info = title + "; " + author + "; " + releaseDate + "; " + pages + "; " + publisher + "; " + isbn;
+        System.out.println(info);
+    }
 }
