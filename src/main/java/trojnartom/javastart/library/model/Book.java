62d7718a1250275bf0be999f1b180f1ff0cdb9c7
trojnartom <trojnar.t@hotmail.com>
2022-03-11 12:19:35 +0100

    Project Library. Call of super().

--- src/main/java/trojnartom/javastart/library/model/Book.java
+++ src/main/java/trojnartom/javastart/library/model/Book.java
@@ -7,9 +7,7 @@ public class Book extends Publication {
     String isbn;
 
     public Book(String title, String author, int year, int pages, String publisher, String isbn) {
-        this.setTitle(title);
-        this.setYear(year);
-        this.setPublisher(publisher);
+        super(title, publisher, year);
         this.author = author;
         this.pages = pages;
         this.isbn = isbn;
